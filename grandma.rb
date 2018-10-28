26e9be002d213cd19d88f8070e9ea65b111f0c58
Jarrian McLean <jarrian.mclean@gmail.com>
2018-10-28 03:40:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,10 @@
 def speak_to_grandma(string)
   if string == string.upcase
       "NO, NOT SINCE 1938!"
+    elsif string == string.upcase
+      return "HUH?! SPEAK UP, SONNY!"
     else
-      "HUH?! SPEAK UP, SONNY!"
+      return "I LOVE YOU TOO PUMPKIN!"
     end
   end
 # Whatever you say to grandma, she should respond with

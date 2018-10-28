fa379fae368f983b360df20c8ac3c230574f106a
Jarrian McLean <jarrian.mclean@gmail.com>
2018-10-28 03:44:53 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
   if string == string.upcase
-      "NO, NOT SINCE 1938!"
+    return  "NO, NOT SINCE 1938!"
     elsif string == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
     else
@@ -10,7 +10,7 @@ def speak_to_grandma(string)
   end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
+# unless you shout it (type in all capitals)
 
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
